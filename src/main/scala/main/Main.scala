cdafb44f7a85cd2955d6f5b20e4b201d42af4a01
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-11-13 20:10:32 -0200

    Permitir realizar multiplas entradas na mesma execução

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -4,13 +4,30 @@ import parser.ImpilerParser
 import parser.ImpilerParser.{parse_input, readInput}
 import types._
 
+import scala.io.StdIn
+
 
 object Main extends App {
-  var parse_result = parse_input(readInput())
-  if(parse_result != null){
-    println("PI-LIB: " + parse_result)
-    var aut:PiAutomata = new PiAutomata(parse_result)
-    aut.solve()
-    aut.printAut()
+  var loop = true
+  while(loop) {
+    var parse_result = parse_input(readInput())
+    if(parse_result != null){
+      println("PI-LIB: " + parse_result)
+      var aut:PiAutomata = new PiAutomata(parse_result)
+      aut.solve()
+      aut.printAut()
+      loop = exitInput()
+    }
+  }
+
+  def exitInput(): Boolean = {
+    print("-----------\nDeseja sair? (s/n)\n ")
+    Console.out.flush()
+    var resposta = StdIn.readLine()
+    if(resposta.equals("s") || resposta.equals("S")){
+      return false
+    }else{
+      return true
+    }
   }
 }
